fde53f3f1803d73fa7051bfb9554a10d44dbfe40
LESANF <nagong1000@naver.com>
2024-12-23 06:08:34 +0900

    wip: createElement, renderElement 구현 중

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,15 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+  console.log(vNode.type, vNode.props, vNode.children);
+  //   Object.
+}
 
 function updateAttributes($el, props) {}
