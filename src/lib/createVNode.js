6fa3e7fdf03ee5bd8cac209fc1cd07de96cda0be
Gyeongmin Kim <gmkim716@gmail.com>
2024-12-22 17:03:10 +0900

    feat: createVNode를 작성합니다
    전달받은 인자를 return 해주는 과정에서 중첩된 자식 요소들을 평탄화해서 전달합니다

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,10 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  const flattenedChildren = children
+    .flat(Infinity)
+    .filter(
+      (child) => child !== null && child !== undefined && child !== false,
+    );
+
+  // children.flat(Infinity): 자식 배열의 중첩된 배열을 완전히 평탄화한다
+  return { type, props, children: flattenedChildren };
 }
