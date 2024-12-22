a71a46a96ae79c064e237db7e87858c57c73ff8b
nogy21 <nogy21@gmail.com>
2024-12-22 22:29:19 +0900

    feat: normalizeVNode 유효성 검증 로직 개선
    - 유효성 로직 수정 및 검증 함수 추가
    - 문자열 반환 함수 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,13 +1,24 @@
 export function normalizeVNode(vNode) {
-  if (vNode === null || vNode === undefined) {
+  if (!validate(vNode)) {
     return "";
   }
-  if (typeof vNode === "boolean") {
-    return "";
-  }
-
-  if (typeof vNode === "number" || typeof vNode === "string") {
+  if (isString(vNode)) {
     return vNode.toString();
   }
+
   return vNode;
+}
+
+/**
+ * @description null, undefined, boolean 일 경우 false 반환
+ */
+function validate(value) {
+  return !(value == null || typeof value === "boolean");
+}
+
+/**
+ * @description 문자열, 숫자일 경우 true 반환
+ */
+function isString(value) {
+  return typeof value === "string" || typeof value === "number";
 }
