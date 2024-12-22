666613c9bafd6a545330fc0f360a5992ae4ddc12
KimBoYoung <qhdud20@naver.com>
2024-12-23 02:27:02 +0900

    feat: eventManager 완료

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,22 +1,25 @@
 const eventsMap = new Map();
+let rootElement = null;
 
 export function addEvent(element, eventType, handler) {
-  const key = `${eventType}-${handler}`;
-  if (!eventsMap.has(key)) {
-    eventsMap.set(key, handler);
+  if (!eventsMap.has(handler)) {
+    eventsMap.set(handler, { eventType, element });
   }
 }
 
 export function removeEvent(element, eventType, handler) {
-  const key = `${eventType}-${handler}`;
-  if (eventsMap.has(key)) {
-    eventsMap.delete(key);
+  if (eventsMap.has(handler)) {
+    if (rootElement) {
+      rootElement.removeEventListener(eventType, handler);
+    }
+    eventsMap.delete(handler);
   }
 }
 
 export function setupEventListeners(root) {
-  eventsMap.forEach((handler, key) => {
-    const [eventType] = key.split("-");
-    root.addEventListener(eventType, handler);
+  rootElement = root;
+
+  eventsMap.forEach(({ eventType }, handler) => {
+    rootElement.addEventListener(eventType, handler);
   });
 }
