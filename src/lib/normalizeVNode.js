c92ce127ebba2cf3b00b5ce3a38d076e15363028
jungeun0712 <kje970712@naver.com>
2024-12-22 17:17:53 +0900

    feat: normalizeVNode typeOf 분기 처리

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,26 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  console.log(vNode);
+  if (
+    vNode === null ||
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean"
+  ) {
+    return "";
+  } else if (typeof vNode === "string") {
+    return vNode;
+  } else if (typeof vNode === "number") {
+    return vNode.toString();
+  } else if (typeof vNode === "function") {
+    const result = vNode();
+    return normalizeVNode(result);
+  } else {
+    return vNode
+      .map((child) => normalizeVNode(child))
+      .filter(
+        (child) =>
+          child === null ||
+          typeof child === "undefined" ||
+          typeof child === "boolean",
+      );
+  }
 }
