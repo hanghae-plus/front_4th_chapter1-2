fc63019d5507204c61d98208cda3540b378a0035
SU_VIN <pyotnqls25@naver.com>
2024-12-23 01:07:55 +0900

    feat: virtualDOM 로직 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,63 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  //null, undefined, boolean
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  //number, string
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return document.createTextNode(String(vNode));
+  }
+
+  //array
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+
+    vNode.forEach((child) => {
+      const childEl = createElement(child);
+      fragment.appendChild(childEl);
+    });
+
+    return fragment;
+  }
+
+  // 함수형 컴포넌트 처리
+  if (typeof vNode.type === "function") {
+    throw new Error(
+      `Function components cannot be directly passed to createElement.`,
+    );
+  }
+
+  const $el = document.createElement(vNode.type);
+
+  if (vNode.props) {
+    updateAttributes($el, vNode.props);
+  }
+
+  if (vNode.children) {
+    vNode.children.forEach((child) => {
+      const childEl = createElement(child);
+      $el.appendChild(childEl);
+    });
+  }
+
+  return $el;
+}
+
+function updateAttributes($el, props) {
+  Object.keys(props).forEach((key) => {
+    if (key.startsWith("on")) {
+      // 이벤트 리스너 처리 (ex: onClick ...)
+      const eventType = key.slice(2).toLowerCase();
+      $el.addEventListener(eventType, props[key]);
+    } else if (key === "className") {
+      // className 처리
+      $el.className = props[key];
+    } else {
+      // 일반 속성 처리
+      $el.setAttribute(key, props[key]);
+    }
+  });
+}
--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,7 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props: props || null,
+    children: children.flat(Infinity).filter((child) => child || child === 0), //falsy값을 제외하고 평탄화
+  };
 }
