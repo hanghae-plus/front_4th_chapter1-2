fded51597af4de49f127e353d6aa19914f2579af
nogy21 <nogy21@gmail.com>
2024-12-22 22:05:17 +0900

    feat: normalizeVNode 값 검증 구현
    - null, undefined, boolean일 경우 빈 문자열 반환
    - 숫자 또는 문자열일 경우 문자열로 반환

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,13 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined) {
+    return "";
+  }
+  if (typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return vNode.toString();
+  }
   return vNode;
 }
