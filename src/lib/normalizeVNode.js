445dc947dd8ba6aa210efa854214d6fd765abc60
Bae seong-kyu <baesense@naver.com>
2024-12-23 00:10:21 +0900

    Feat : 컴포넌트 정규화 & falsy 필터

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -2,8 +2,20 @@ export function normalizeVNode(vNode) {
   if (typeof vNode === "number" || typeof vNode === "string") {
     return String(vNode);
   }
-  if (!vNode || typeof vNode === "boolean") {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
     return "";
   }
-  return vNode;
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({ ...vNode.props, children: vNode.children }),
+    );
+  }
+  return {
+    ...vNode,
+    children: vNode.children
+      .filter((child) => child || child === 0)
+      .map((child) => normalizeVNode(child))
+      .filter((child) => child !== ""),
+  };
 }
