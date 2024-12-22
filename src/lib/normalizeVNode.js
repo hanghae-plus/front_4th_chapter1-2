22d15185562817e297ecd9a1a0b1f85461203134
LESANF <nagong1000@naver.com>
2024-12-23 06:05:12 +0900

    feat: normalizeVNode 컨디션 추가 함수 작성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    const renderedElement = vNode.type({
+      ...vNode.props,
+      children: vNode.children,
+    });
+    return normalizeVNode(renderedElement);
+  }
+
   return vNode;
 }
