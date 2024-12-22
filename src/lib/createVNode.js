c3a9953aa6dbda23e9619a0acafc7145c3896ea9
nogy21 <nogy21@gmail.com>
2024-12-22 22:00:51 +0900

    feat: createVNode 함수 반환값 중 children 요소 평탄화 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -2,6 +2,14 @@ export function createVNode(type, props, ...children) {
   return {
     type,
     props,
-    children: children.filter((child) => (child === 0 || child ? true : false)),
+    children: children
+      .flat(Infinity)
+      .filter((child) => (child === 0 || child ? true : false))
+      .flatMap((child) => {
+        if (child instanceof Array) {
+          return child;
+        }
+        return child;
+      }),
   };
 }
