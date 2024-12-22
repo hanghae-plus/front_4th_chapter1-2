9d123b38bfe9b4e32a3abc4c26562843c1eeb034
Bae seong-kyu <baesense@naver.com>
2024-12-22 17:19:33 +0900

    Refactor : 변수 없이 바로 리턴하도록 수정

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,11 +1,7 @@
 export function createVNode(type, props, ...children) {
-  const flatChildren = children
-    .flat(Infinity)
-    .filter((child) => child || child === 0);
-
   return {
     type,
-    props: props || null,
-    children: flatChildren,
+    props: props,
+    children: children.flat(Infinity).filter((child) => child || child === 0),
   };
 }
