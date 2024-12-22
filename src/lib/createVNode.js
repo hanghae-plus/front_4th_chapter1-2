1492b27f5e263ecd71b6b1250c5106968695d129
9yurilee <9yurilee@gmail.com>
2024-12-23 00:29:31 +0900

    feat: createVNode 작성 완료

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,11 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  // 1. type, props, ...children을 매개변수로 받는 함수를 작성하세요.
+  // 1. 반환값은 { type, props, children } 형태의 객체여야 합니다.
+  // 2. children은 평탄화(flat)되어야 하며, 0을 제외한 falsy 값은 필터링되어야 합니다.
+
+  return {
+    type, // 태그명
+    props, // id, className 등 속성. 없을 수도 있음!
+    children: children.flat(Infinity).filter((child) => child || child === 0),
+  };
 }
