a2dbb1cd20f1f461772f62fd9606f6131a765e96
9yurilee <9yurilee@gmail.com>
2024-12-23 00:35:17 +0900

    feat: createElement 함수 작성 완료

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,45 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  // 1. vNode가 null, undefined, boolean 면 빈 텍스트 노드를 반환합니다.
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  // 2. vNode가 문자열이나 숫자면 텍스트 노드를 생성하여 반환합니다.
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  // 3. vNode가 배열이면 DocumentFragment를 생성하고 각 자식에 대해 createElement를 재귀 호출하여 추가합니다.
+  if (Array.isArray(vNode)) {
+    const docFragment = document.createDocumentFragment();
+
+    vNode.forEach((el) => docFragment.appendChild(createElement(el)));
+
+    return docFragment;
+  }
+
+  // 4. 위 경우가 아니면 실제 DOM 요소를 생성합니다
+  // - vNode.type에 해당하는 요소를 생성
+  const element = document.createElement(vNode.type);
+  // - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
+  updateAttributes(element, vNode.props || {});
+  // - vNode.children의 "각 자식"에 대해 createElement를 재귀 호출하여 추가
+  element.append(...vNode.children.map(createElement));
+
+  return element;
+}
+
+// vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
+function updateAttributes($el, props) {
+  Object.entries(props).forEach(([attr, value]) => {
+    if (attr.startsWith("on") && typeof value === "function") {
+      addEvent($el, attr.slice(2).toLowerCase(), value);
+    } else if (attr === "className") {
+      $el.setAttribute("class", value);
+    } else {
+      $el.setAttribute(attr, value);
+    }
+  });
+}
