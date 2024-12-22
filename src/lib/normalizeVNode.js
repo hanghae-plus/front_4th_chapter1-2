42eefe3f5e7eed689c6ec595a7f2f487659e5e9e
chb6734 <chb6734@gmail.com>
2024-12-23 02:53:35 +0900

    feat: VNode 테스트 케이스 통과

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,11 @@
 export function normalizeVNode(vNode) {
+  if (
+    typeof vNode === "boolean" ||
+    typeof vNode === "undefined" ||
+    vNode === null
+  ) {
+    return "";
+  }
+  console.log(vNode);
   return vNode;
 }
