1648acce0a87126e0671614a55024c3afd7632d1
nogy21 <nogy21@gmail.com>
2024-12-24 02:30:02 +0900

    feat: updateElement 함수에 가상 DOM 비교, DOM 업데이트 및 updateAttributes 함수 로직 구현
    - updateElement 함수 구현
      - 가상 DOM 비교하여 DOM 조작
      - 가상 DOM 속성 변경 시 updateAttributes 함수 호출
      - 가상 DOM 자식 요소 비교 시 재귀적 호출
    - updateAttributes 함수 구현
      - on<event> 등록 및 제거 시 전역 관리를 위한 addEvent, removeEvent 호출
      - 속성 비교 후 DOM 반영

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,99 @@
-import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
+import { addEvent, removeEvent } from "./eventManager";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+function updateAttributes(target, originNewProps, originOldProps) {
+  if (!target) {
+    return;
+  }
+  const newProps = { ...originNewProps };
+  const oldProps = { ...originOldProps };
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+  const props = { ...newProps, ...oldProps };
+
+  Object.keys(props).forEach((key) => {
+    if (key.startsWith("on")) {
+      const eventType = key.slice(2).toLowerCase();
+      if (newProps[key] && !oldProps[key]) {
+        addEvent(target, eventType, newProps[key]);
+      } else if (!newProps[key] && oldProps[key]) {
+        removeEvent(target, eventType, oldProps[key]);
+      }
+      return;
+    }
+
+    if (newProps[key] === oldProps[key]) {
+      return;
+    }
+
+    if (!newProps[key]) {
+      target.removeAttribute(key);
+      return;
+    }
+
+    if (key === "className") {
+      target.setAttribute("class", newProps[key]);
+      return;
+    }
+
+    target.setAttribute(key, newProps[key]);
+  });
+}
+
+/**
+ * @description 기존 DOM을 업데이트하는 함수
+ * @param {*} parentElement
+ * @param {*} newNode
+ * @param {*} oldNode
+ * @param {*} index
+ * @returns {void}
+ */
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  if (oldNode && !newNode) {
+    return parentElement.removeChild(parentElement.childNodes[index]);
+  }
+
+  if (!oldNode && newNode) {
+    return parentElement.appendChild(createElement(newNode));
+  }
+
+  if (typeof newNode === "string" && typeof oldNode === "string") {
+    if (newNode === oldNode) {
+      return;
+    }
+    return parentElement.replaceChild(
+      createElement(newNode),
+      parentElement.childNodes[index],
+    );
+  }
+
+  if (newNode.type !== oldNode.type) {
+    return parentElement.replaceChild(
+      createElement(newNode),
+      parentElement.childNodes[index],
+    );
+  }
+
+  if (newNode.type === oldNode.type) {
+    updateAttributes(
+      parentElement.childNodes[index],
+      newNode.props || {},
+      oldNode.props || {},
+    );
+  }
+
+  const newLength = newNode.children?.length || 0;
+  const oldLength = oldNode.children?.length || 0;
+  const maxLength = Math.max(newLength, oldLength);
+
+  for (let i = 0; i < maxLength; i++) {
+    if (!parentElement.childNodes[index]) {
+      return;
+    }
+    updateElement(
+      parentElement.childNodes[index],
+      newNode.children[i],
+      oldNode.children[i],
+      i,
+    );
+  }
+}
