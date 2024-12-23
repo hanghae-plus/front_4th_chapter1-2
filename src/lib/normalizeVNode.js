7f6a557cc28c834dec73d355f673311315e5a94f
Blue-Kite <bluelemon112@gmail.com>
2024-12-23 21:38:42 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,26 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (
+    vNode == null ||
+    typeof vNode === "undefined" ||
+    typeof vNode === "boolean"
+  ) {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({ ...vNode.props, children: vNode.children }),
+    );
+  }
+
+  return {
+    ...vNode,
+    children: vNode.children
+      .map((child) => normalizeVNode(child))
+      .filter((child) => child || child === 0),
+  };
 }
