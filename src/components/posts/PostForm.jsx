317a250125f35842d9e60fb90d02271e05c3d79d
Geunbaek <rmsqor103@naver.com>
2024-12-23 21:41:41 +0900

    feat: post action 적용

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,20 +1,35 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores/globalStore";
 
 export const PostForm = () => {
+  const { addPost } = globalStore.actions;
+  const { currentUser } = globalStore.getState();
+
+  const handleAddPost = (event) => {
+    event.preventDefault();
+    const content = new FormData(event.target).get("postContent");
+    addPost(content);
+  };
+
   return (
-    <div className="mb-4 bg-white rounded-lg shadow p-4">
+    <form
+      className="mb-4 bg-white rounded-lg shadow p-4"
+      onSubmit={handleAddPost}
+    >
       <textarea
         id="post-content"
+        name="postContent"
         placeholder="무슨 생각을 하고 계신가요?"
         className="w-full p-2 border rounded"
       />
       <button
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+        type="submit"
       >
         게시
       </button>
-    </div>
+    </form>
   );
 };
