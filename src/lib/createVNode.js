9c363c19508dedd6501ffb116c6ac2486c3c08d2
LESANF <nagong1000@naver.com>
2024-12-24 06:02:18 +0900

    fix: createVNode test 기반 props 초기 기본 값 제거

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -2,12 +2,16 @@ export function createVNode(type, props, ...children) {
   const filteredVNodeChildren = children
     .flat(Infinity)
     .filter(
-      (child) => child !== null && child !== undefined && child !== false,
+      (child) =>
+        child !== null &&
+        child !== undefined &&
+        child !== false &&
+        child !== true,
     );
 
   const vNode = {
     type,
-    props: props || {},
+    props,
     children: filteredVNodeChildren,
   };
 
