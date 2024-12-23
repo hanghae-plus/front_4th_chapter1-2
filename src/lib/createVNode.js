86ca842c7aabece4784677f30726f0bc966f709c
Geunbaek <rmsqor103@naver.com>
2024-12-23 15:42:35 +0900

    feat: createVNode 함수 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,10 @@
+const isRenderedVNode = (vNode) =>
+  vNode !== null && vNode !== undefined && typeof vNode !== "boolean";
+
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children.flat(Infinity).filter(isRenderedVNode),
+  };
 }
