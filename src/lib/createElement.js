b2058cd509a98c4760db1a8d37a926280ffa7c99
Geunbaek <rmsqor103@naver.com>
2024-12-23 15:47:49 +0900

    feat: createElement 함수 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,47 @@
 import { addEvent } from "./eventManager";
+import { isRenderedVNode, isTextVNode } from "./util";
 
-export function createElement(vNode) {}
+const isEvent = (key) => key.startsWith("on");
+const isClassName = (key) => key === "className";
 
-function updateAttributes($el, props) {}
+export function createElement(vNode) {
+  if (!isRenderedVNode(vNode)) {
+    return document.createTextNode("");
+  }
+
+  if (isTextVNode(vNode)) {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    fragment.append(...Array.from(vNode).map(createElement));
+    return fragment;
+  }
+
+  const { type, props, children } = vNode;
+  const element = document.createElement(type);
+  updateAttributes(element, props);
+
+  element.append(...Array.from(children).map(createElement));
+
+  return element;
+}
+
+function updateAttributes($el, props) {
+  Object.keys(props ?? {}).forEach((key) => {
+    if (isEvent(key)) {
+      const eventType = key.slice(2).toLowerCase();
+      const handler = props[key];
+      addEvent($el, eventType, handler);
+      return;
+    }
+
+    if (isClassName(key)) {
+      $el.setAttribute("class", props[key]);
+      return;
+    }
+
+    $el.setAttribute(key, props[key]);
+  });
+}
