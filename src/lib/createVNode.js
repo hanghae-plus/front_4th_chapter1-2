660d2b648e935abde2fcf00dd59c5d59723ea7c5
pitang <wpeffect@gmail.com>
2024-12-23 19:06:29 +0900

    feat: 여러 자식 처리 및 자식 배열 평탄화

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,17 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type, // 노드의 타입
+    props, // 해당 노드의 적용할 속성
+    children: children // 이 노드의 자식 요소들
+      .flat(Infinity) // 모든 중첩 배열을 평탄화함.
+      .filter(
+        // 자식 요소에서 0을 제외한 falsy값을 제거
+        (child) =>
+          child !== false &&
+          child !== null &&
+          child !== undefined &&
+          child !== "" &&
+          !Number.isNaN(child),
+      ),
+  };
 }
