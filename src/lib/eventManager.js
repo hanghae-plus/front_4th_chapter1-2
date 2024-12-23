b70cea1ee5cdfcc652ddae4de5d88af1e9f961f2
nogy21 <nogy21@gmail.com>
2024-12-23 18:36:00 +0900

    feat: eventManager 이벤트 위임 등록
    - 상위 스코프의 eventListeners 사용하여 이벤트 추가 및 등록 관리

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,17 @@
-export function setupEventListeners(root) {}
+const eventListeners = new Set();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  eventListeners.forEach(({ element, eventType, handler }) => {
+    root.addEventListener(eventType, (e) => {
+      if (element === e.target) {
+        handler(e);
+      }
+    });
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  eventListeners.add({ element, eventType, handler });
+}
+
+// export function removeEvent(element, eventType, handler) {}
