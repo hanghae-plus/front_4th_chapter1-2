ad4bfdd6e95a09a7083800466c489edbad068ceb
KimYoungWoo <rladuddn3924@gmail.com>
2024-12-23 16:14:26 +0900

    FEAT: normalizeVNode 함수 내용 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,19 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (!vNode || typeof vNode === "boolean") {
+    return "";
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return `${vNode}`;
+  }
+
+  if (typeof vNode === "function") {
+    const component = vNode();
+    return normalizeVNode(component);
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode.map(normalizeVNode).join("");
+  }
+
+  return normalizeVNode(vNode.children);
 }
