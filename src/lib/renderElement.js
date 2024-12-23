14b106b7f2b1adee5b51722c96cb631e158ca17d
Geunbaek <rmsqor103@naver.com>
2024-12-23 21:35:54 +0900

    feat: updateElement 함수 추가 및 적용

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,17 +1,30 @@
-import { cleanupEventManager, setupEventListeners } from "./eventManager";
+import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
+import { updateElement } from "./updateElement";
+
+let virtualDOM = null;
 
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
-  cleanupEventManager(container);
-  const element = createElement(normalizeVNode(vNode));
-  if (container.firstChild) {
-    container.replaceChild(element, container.firstChild);
-  } else {
+
+  const normalizedVNode = normalizeVNode(vNode);
+  if (virtualDOM === null) {
+    const element = createElement(normalizedVNode);
     container.append(element);
+    virtualDOM = normalizedVNode;
+    return;
+  }
+
+  if (container.firstChild) {
+    updateElement(container, normalizedVNode, virtualDOM);
+    virtualDOM = normalizedVNode;
+  } else {
+    const element = createElement(normalizedVNode);
+    container.append(element);
+    virtualDOM = normalizedVNode;
   }
   setupEventListeners(container);
 }
--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -1,6 +1,106 @@
 import { addEvent, removeEvent } from "./eventManager";
 import { createElement } from "./createElement.js";
+import { isClassName, isEvent } from "./util.js";
 
-function updateAttributes(target, originNewProps, originOldProps) {}
+function isChangedAttributes(originNewProps, originOldProps) {
+  if (
+    (!originNewProps && originOldProps) ||
+    (originNewProps && !originOldProps)
+  ) {
+    return true;
+  }
 
-export function updateElement(parentElement, newNode, oldNode, index = 0) {}
+  const mergesProps = { ...originOldProps, ...originNewProps };
+  return Object.keys(mergesProps ?? {}).some(
+    (key) => mergesProps[key] !== originOldProps[key],
+  );
+}
+
+function updateAttributes(target, originNewProps, originOldProps) {
+  Object.keys(originOldProps ?? {}).forEach((key) => {
+    if (isEvent(key)) {
+      const eventType = key.slice(2).toLowerCase();
+      removeEvent(target, eventType);
+      return;
+    }
+
+    if (isClassName(key)) {
+      target.removeAttribute("class");
+      return;
+    }
+
+    target.removeAttribute(key);
+  });
+
+  Object.keys(originNewProps ?? {}).forEach((key) => {
+    if (isEvent(key)) {
+      const eventType = key.slice(2).toLowerCase();
+      addEvent(target, eventType, originNewProps[key]);
+      return;
+    }
+
+    if (isClassName(key)) {
+      target.setAttribute("class", originNewProps[key]);
+      return;
+    }
+
+    target.setAttribute(key, originNewProps[key]);
+  });
+}
+
+export function updateElement(parentElement, newNode, oldNode, index = 0) {
+  // 새로운 노드가 추가될 경우
+  if (newNode && !oldNode) {
+    parentElement.appendChild(createElement(newNode));
+    return;
+  }
+
+  // 이전 노드가 삭제된 경우
+  if (!newNode && oldNode) {
+    parentElement.removeChild(parentElement.childNodes[index]);
+    return;
+  }
+
+  // 노드의 타입이 다른경우
+  if (newNode.type !== oldNode.type) {
+    parentElement.replaceChild(
+      createElement(newNode),
+      parentElement.childNodes[index],
+    );
+    return;
+  }
+
+  // 텍스트 노드일 경우
+  if (
+    typeof newNode === "string" &&
+    typeof oldNode === "string" &&
+    newNode !== oldNode
+  ) {
+    parentElement.replaceChild(
+      createElement(newNode),
+      parentElement.childNodes[index],
+    );
+    return;
+  }
+
+  if (isChangedAttributes(newNode.props, oldNode.props)) {
+    updateAttributes(
+      parentElement.childNodes[index],
+      newNode.props,
+      oldNode.props,
+    );
+  }
+
+  const newNodeChildren = newNode.children ?? [];
+  const oldNodeChildren = oldNode.children ?? [];
+  const maxChildren = Math.max(newNodeChildren.length, oldNodeChildren.length);
+
+  for (let i = 0; i < maxChildren; i++) {
+    updateElement(
+      parentElement.childNodes[index],
+      newNodeChildren[i],
+      oldNodeChildren[i],
+      i,
+    );
+  }
+}
