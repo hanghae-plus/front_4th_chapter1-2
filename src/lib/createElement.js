41bc9e3e8c608a155a0ad8387fb17ae159305844
KimYoungWoo <rladuddn3924@gmail.com>
2024-12-23 15:58:20 +0900

    FEAT: createElement 함수 내용 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,33 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (!vNode || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(`${vNode}`);
+  }
+  if (Array.isArray(vNode)) {
+    const $fragment = document.createDocumentFragment();
+    vNode.forEach((child) => {
+      $fragment.appendChild(createElement(child));
+    });
+    return $fragment;
+  }
 
-function updateAttributes($el, props) {}
+  const $el = document.createElement(vNode.type, vNode.props);
+  updateAttributes($el, vNode.props);
+  vNode.children.forEach((child) => $el.appendChild(createElement(child)));
+  return $el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props || {}).forEach(([key, value]) => {
+    if (key.startsWith("on")) {
+      const eventType = key.toLowerCase().substring(2);
+      addEvent(eventType, value);
+    } else {
+      $el.setAttribute(key, value);
+    }
+  });
+}
