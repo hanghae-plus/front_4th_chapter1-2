42f17ef5ca082e3b44986b36aaf5a25e7b4ab5ba
LESANF <nagong1000@naver.com>
2024-12-24 06:04:21 +0900

    chore: eslint no-unused-vars off 처리

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,11 @@ import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
   eslintPluginPrettier,
   eslintConfigPrettier,
 ];
