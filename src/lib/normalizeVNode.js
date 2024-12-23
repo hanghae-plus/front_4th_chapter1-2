18a6f1e2ef48cde8956d7d2252bad8310c216d2d
Geunbaek <rmsqor103@naver.com>
2024-12-23 15:46:13 +0900

    feat: normalizeVNode 함수 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,26 @@
+import { isRenderedVNode } from "./util";
+
+const isTextVNode = (vNode) =>
+  typeof vNode === "string" || typeof vNode === "number";
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (!isRenderedVNode(vNode)) {
+    return "";
+  }
+
+  if (isTextVNode(vNode)) {
+    return vNode.toString();
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({ ...vNode.props, children: vNode.children }),
+    );
+  }
+
+  return {
+    type: vNode.type,
+    props: vNode.props,
+    children: Array.from(vNode.children).map(normalizeVNode),
+  };
 }
