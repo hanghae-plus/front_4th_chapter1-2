a9947a8b517006370208b0887b5fbc5c48198f21
YeongseoYoon-hanghae <btbb@naver.com>
2024-12-23 19:43:40 +0900

    feat: createVNode 함수 개선 및 타입 정의 추가

--- src/libs/createVNode.ts
+++ src/libs/createVNode.ts
@@ -1,3 +1,23 @@
-export function createVNode(type, props, ...children) {
-  return {};
+type Props = {
+  [key: string]: any;
+  children?: VNode[];
+};
+
+type VNode = {
+  type: string;
+  props: Props;
+};
+
+export function createVNode(
+  type: string,
+  props: Props | null,
+  ...children: any[]
+): VNode {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.flat(),
+    },
+  };
 }
