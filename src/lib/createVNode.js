72bd3938868e5251f4bf7fc192450d43b6fa085f
CodyMan0 <hys83751952@gmail.com>
2024-12-23 20:48:04 +0900

    :sparkles: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,12 @@
+/**
+ * 가상돔 노드 생성하는 역할
+ * 조건 : children은 평탄화(flat)되어야 하며, 0을 제외한 Falsy는 필터
+ */
+
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props: props || null,
+    children: children.flat(Infinity).filter((child) => child === 0 || child),
+  };
 }
