c849722efdfb21d497dae1e03ef9f89ccab3ebb6
nopinokio <nopinokio90@gmail.com>
2024-12-24 02:15:14 +0900

    feat: normalizeVNode 함수 추가

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,27 @@
+import { isValidVnode } from "./validCheck";
+
 export function normalizeVNode(vNode) {
-  return vNode;
+  //  null, undefined, falsy 의 경우 빈 텍스트 return
+  if (!isValidVnode(vNode)) {
+    return "";
+  }
+
+  // 문자열, 숫자일 경우 string 노드로 return
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  // 함수형태 처리
+  if (typeof vNode.type === "function") {
+    const resolvedVNode = vNode.type(vNode.props || {});
+    return normalizeVNode(resolvedVNode);
+  }
+
+  return {
+    type: vNode.type,
+    props: vNode.props || {},
+    children: Array.isArray(vNode.children)
+      ? vNode.children.filter(Boolean).map(normalizeVNode)
+      : [],
+  };
 }
