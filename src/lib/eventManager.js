fd03adaed75f57d44cbbecf9e6ce612256eb6903
nogy21 <nogy21@gmail.com>
2024-12-23 21:12:39 +0900

    feat: eventManager에서 이벤트 리스너 배열로 변경 및 제거 기능 추가
    - eventListeners의 손쉬운 관리를 위해 배열로 자료구조 변경
      - 중복 제거 없이 특정 인덱스에 접근/수정/제거를 쉽게 하기 위함
      - 추후 최적화를 한다면 다른 자료구조 사용
    - addEventListener와 removeEventListener에 동일한 이벤트핸들러 참조를 위해 boundHandler 구현 및 eventListeners에 갱신

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,17 +1,34 @@
-const eventListeners = new Set();
+const eventListeners = [];
 
 export function setupEventListeners(root) {
-  eventListeners.forEach(({ element, eventType, handler }) => {
-    root.addEventListener(eventType, (e) => {
+  eventListeners.forEach(({ element, eventType, handler }, i) => {
+    const boundHandler = (e) => {
       if (element === e.target) {
         handler(e);
       }
-    });
+    };
+
+    root.addEventListener(eventType, boundHandler);
+    eventListeners[i] = {
+      element,
+      eventType,
+      handler,
+      remove: () => root.removeEventListener(eventType, boundHandler),
+    };
   });
 }
 
 export function addEvent(element, eventType, handler) {
-  eventListeners.add({ element, eventType, handler });
+  eventListeners.push({ element, eventType, handler });
 }
 
-// export function removeEvent(element, eventType, handler) {}
+export function removeEvent(element, eventType, handler) {
+  const deleteIndex = eventListeners.findIndex(
+    (event) =>
+      event.element === element &&
+      event.eventType === eventType &&
+      event.handler === handler,
+  );
+  eventListeners[deleteIndex].remove();
+  eventListeners.splice(deleteIndex, 1);
+}
