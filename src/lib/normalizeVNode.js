4e935ee2170b7155bfffb35caec5294cd03b3c5f
pitang <wpeffect@gmail.com>
2024-12-24 01:53:15 +0900

    feat: normallizeVNode 함수 작성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,42 @@
 export function normalizeVNode(vNode) {
+  // null, undefined, boolean 값은 빈 문자열로 변환
+  if (
+    vNode === null ||
+    vNode === undefined ||
+    vNode === true ||
+    vNode === false
+  ) {
+    return "";
+  }
+
+  // 문자열과 숫자는 문자열로 변환
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  // 컴포넌트를 정규화
+  // 배열처리 중첩 배열을 평탄화하고, 모든 요소를 재귀적으로 정규화
+  if (Array.isArray(vNode)) {
+    return vNode.flat(Infinity).map(normalizeVNode);
+  }
+
+  // 사용자 정의 컴포넌트 처리 (컴포넌트 정규화)
+  if (typeof vNode.type === "function") {
+    const result = vNode.type({
+      ...vNode.props,
+      children: normalizeVNode(vNode.children),
+    });
+    return normalizeVNode(result);
+  }
+
+  // 기본 노드 처리
+  if (typeof vNode.type === "string") {
+    return {
+      type: vNode.type,
+      props: vNode.props,
+      children: vNode.children.flat(Infinity).filter(normalizeVNode),
+    };
+  }
+
   return vNode;
 }
