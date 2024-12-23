235a9a8014e2b4b53bce0f4029adb11fe9ddae76
KimYoungWoo <rladuddn3924@gmail.com>
2024-12-23 16:17:11 +0900

    FEAT: eventManager 함수 내용 추가

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,31 @@
-export function setupEventListeners(root) {}
+const eventHandler = {};
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  // TODO: 이벤트 함수를 가져와서 한 번에 root에 이벤트를 등록한다.
+  Object.keys(eventHandler).forEach((eventType) => {
+    root.addEventListener(eventType, (e) => {
+      const handlers = eventHandler[eventType];
+      const target = e.target;
 
-export function removeEvent(element, eventType, handler) {}
+      if (handlers[target]) {
+        handlers[target](e);
+      }
+    });
+  });
+}
+
+export function addEvent(element, eventType, handler) {
+  // element에 대한 이벤트 함수를 어딘가에 저장한다
+  if (!eventHandler[element]) {
+    eventHandler[element] = {};
+  }
+  eventHandler[element][eventType] = handler;
+}
+
+export function removeEvent(element, eventType) {
+  // element에 대한 이벤트 함수를 어딘가에 삭제한다
+  if (!eventHandler[element]) {
+    return;
+  }
+  delete eventHandler[element][eventType];
+}
