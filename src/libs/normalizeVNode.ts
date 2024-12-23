247bb1203125d90a2ee73c0170913cdf8a56edd2
YeongseoYoon-hanghae <btbb@naver.com>
2024-12-23 21:47:59 +0900

    feat: normalizeVNode 함수에 문자열 처리 추가 및 타입 정의 개선

--- src/libs/normalizeVNode.ts
+++ src/libs/normalizeVNode.ts
@@ -1,3 +1,20 @@
-export function normalizeVNode(vNode) {
+import { VNode } from "../types";
+
+/**
+ * Virtual Node를 정규화하는 함수
+ * @param vNode - 정규화할 Virtual Node
+ * @returns 정규화된 Virtual Node
+ */
+export function normalizeVNode(vNode: VNode | string) {
+  if (typeof vNode === "string") {
+    return {
+      type: "text",
+      props: {
+        nodeValue: vNode,
+        children: [],
+      },
+    };
+  }
+
   return vNode;
 }
