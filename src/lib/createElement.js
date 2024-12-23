799f22cd439b00a1dc2e1edd80f33e9abdf0c563
KimBoYoung <qhdud20@naver.com>
2024-12-23 21:41:26 +0900

    feat: renderElement 완료

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,4 +1,4 @@
-// import { addEvent } from "./eventManager";
+import { addEvent } from "./eventManager";
 
 export function createElement(vNode) {
   if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
@@ -16,16 +16,7 @@ export function createElement(vNode) {
   const $el = document.createElement(vNode.type);
 
   if (vNode.props) {
-    for (const [key, value] of Object.entries(vNode.props)) {
-      console.log(key, value);
-      if (key.startsWith("on")) {
-        $el.addEventListener(key.slice(2).toLowerCase(), value);
-      } else if (key === "className") {
-        $el.className = value;
-      } else {
-        $el.setAttribute(key, value);
-      }
-    }
+    updateAttributes($el, vNode.props);
   }
 
   if (vNode.children) {
@@ -35,4 +26,14 @@ export function createElement(vNode) {
   return $el;
 }
 
-// function updateAttributes($el, props) {}
+function updateAttributes($el, props) {
+  for (const [key, value] of Object.entries(props)) {
+    if (key.startsWith("on")) {
+      addEvent($el, key.slice(2).toLowerCase(), value);
+    } else if (key === "className") {
+      $el.className = value;
+    } else {
+      $el.setAttribute(key, value);
+    }
+  }
+}
--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -9,14 +9,25 @@ export function addEvent(element, eventType, handler) {
 
 export function removeEvent(element, eventType, handler) {
   if (eventsMap.has(handler)) {
-    if (rootElement) {
-      rootElement.removeEventListener(eventType, handler);
+    const { eventType: storedEventType, element: storedElement } =
+      eventsMap.get(handler);
+
+    if (storedEventType === eventType && storedElement === element) {
+      if (rootElement) {
+        rootElement.removeEventListener(eventType, handler);
+      }
+      eventsMap.delete(handler);
     }
-    eventsMap.delete(handler);
   }
 }
 
 export function setupEventListeners(root) {
+  if (rootElement) {
+    eventsMap.forEach(({ eventType }, handler) => {
+      rootElement.removeEventListener(eventType, handler);
+    });
+  }
+
   rootElement = root;
 
   eventsMap.forEach(({ eventType }, handler) => {
