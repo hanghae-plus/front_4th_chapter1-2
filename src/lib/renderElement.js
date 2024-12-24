2ee9414fdc2e0559b01a6e053f9ff7def1b71f1e
장원정 <tazoal@tazoal.local>
2024-12-24 20:09:04 +0900

    refactor: oldNode 관리를 변수에서 WeakMap으로 변경

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,22 +3,20 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
-let oldNode = null;
+const nodeToContainerMap = new WeakMap();
 
 export function renderElement(vNode, container) {
   vNode = normalizeVNode(vNode);
 
-  if (!container.childNodes[0]) {
-    oldNode = null;
-  }
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  if (!oldNode) {
+  if (!nodeToContainerMap.has(container)) {
     container.append(createElement(vNode));
   } else {
     // 이후에는 updateElement로 기존 DOM을 업데이트한다.
+    const oldNode = nodeToContainerMap.get(container);
     updateElement(container, vNode, oldNode);
   }
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
   setupEventListeners(container);
-  oldNode = vNode;
+  nodeToContainerMap.set(container, vNode);
 }
