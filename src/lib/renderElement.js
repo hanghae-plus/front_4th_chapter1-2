dfa63d67a53c0d58c5c9b1621a5f65e203d2b340
jch1223 <ico1828@gmail.com>
2024-12-26 05:17:22 +0900

    renderElement 로직

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -4,7 +4,8 @@ import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  // TODO:  updateElement로 기존 DOM을 업데이트한다.
+  container.innerHTML = null;
+  container.append(createElement(normalizeVNode(vNode)));
+  setupEventListeners(container);
 }
