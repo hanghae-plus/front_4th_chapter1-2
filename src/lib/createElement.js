33ffa5fa6b8bd7de5f00b30424fb8b01afab638d
chb6734 <chb6734@gmail.com>
2024-12-26 02:39:21 +0900

    feat: createElement테스트 통과

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,54 @@
-import { addEvent } from "./eventManager";
+//import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  // undifined, null, boolean은 빈텍스트 노드로 변환
+  if (
+    typeof vNode === "boolean" ||
+    vNode === null ||
+    typeof vNode === "undefined"
+  ) {
+    return document.createTextNode("");
+  }
 
-function updateAttributes($el, props) {}
+  //string과 number은 텍스트 노드로 변환
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  // vNode가 배열일 때 fragment로 리턴
+  if (Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+    vNode.forEach((node) => {
+      console.log("test", node);
+      fragment.append(createElement(node));
+    });
+    return fragment;
+  }
+  // 컴포넌트를 createElement로 처리하려고 하면 오류 발생
+  if (typeof vNode === "object" && typeof vNode === "function") {
+    throw new Error("error");
+  }
+
+  //컴포넌트 정규화
+  const { type, props, children } = vNode;
+  const $el = document.createElement(type);
+  updateAttributes($el, props);
+
+  if (children) {
+    children.forEach((child) => {
+      const childElement = createElement(child);
+      $el.append(childElement);
+    });
+  }
+  return $el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props || {}).forEach(([key, value]) => {
+    if (key === "className") {
+      $el.classList = value;
+      return;
+    }
+    $el.setAttribute(key, value);
+  });
+}
