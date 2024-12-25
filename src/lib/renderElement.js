e26bbb58056047461b11516400a20c51f309a4bf
JunmanChoi <junman9595@gmail.com>
2024-12-26 01:46:49 +0900

    fix: 생성 로직 순서 개선 및 초기화 코드 제거를 통해 1-1 이슈 해결(원인 파악 필요)

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,6 +1,5 @@
 import { createElement } from "./createElement";
-import { createElementChildRemoveObserver } from "./elementObserver";
-import { clearEvents, setupEventListeners } from "./eventManager";
+import { setupEventListeners } from "./eventManager";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
@@ -12,38 +11,27 @@ let oldVNode = null;
  * @param {HTMLElement} container 실제 DOM 엘리먼트(해당 VNode를 렌더링할 대상)
  */
 export function renderElement(vNode, container) {
-  if (!container.children[0]) {
-    initOldVNode();
-  }
-
-  // step1. vNode 정규화
   if (!vNode) {
     return;
   }
-  const normalizedVNode = normalizeVNode(vNode);
+  // step1. vNode 정규화
+  const newVNode = normalizeVNode(vNode);
 
   // step2. 정규화된 vNode를 기반으로 DOM 생성 또는 업데이트
   if (!oldVNode) {
     // step3. DOM 추가
-    const element = createElement(normalizedVNode);
+    const element = createElement(newVNode);
     container.append(element); // 첫 렌더링 때만 container에 DOM 추가
-    // 예외처리 :  DOM 삭제 감지
-    createElementChildRemoveObserver([container, document.body], () => {
-      initOldVNode();
-      clearEvents();
-    });
+  } else {
+    if (container.firstChild) {
+      updateElement(container, newVNode, oldVNode);
+    } else {
+      const element = createElement(newVNode);
+      container.append(element);
+    }
   }
-
-  // step3. DOM 업데이트
-  oldVNode && updateElement(container, normalizedVNode, oldVNode);
-
   // step4. 이벤트 핸들러 등록
   setupEventListeners(container);
-
   // step5. oldVNode 업데이트
-  oldVNode = normalizedVNode;
-}
-
-function initOldVNode() {
-  oldVNode = null;
+  oldVNode = newVNode;
 }
