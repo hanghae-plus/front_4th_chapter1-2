c7aff29879b2162ec7345acdda360181aa64feff
jungeun0712 <kje970712@naver.com>
2024-12-25 20:13:49 +0900

    feat: eventManager 기능 추가

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,25 @@
-export function setupEventListeners(root) {}
+const eventArray = [];
+let $el = null;
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  $el = root;
+  eventArray.forEach((event) => {
+    root.addEventListener(event.eventType, event.handler);
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+export function addEvent(element, eventType, handler) {
+  eventArray.push({ element, eventType, handler });
+}
+
+export function removeEvent(element, eventType, handler) {
+  const index = eventArray.findIndex(
+    ({ element: el, eventType: et, handler: h }) =>
+      el === element && et === eventType && h === handler,
+  );
+  if (index !== -1) {
+    eventArray.splice(index, 1);
+  }
+
+  $el.removeEventListener(eventType, handler);
+}
