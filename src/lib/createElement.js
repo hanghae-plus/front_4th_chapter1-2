a6e1d2f9f11bc993ae56e6612e15a69c83a9d43b
jch1223 <ico1828@gmail.com>
2024-12-26 01:40:27 +0900

    파라비터가 배열일 경우 로직

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -12,6 +12,26 @@ export function createElement(vNode) {
   if (typeof vNode === "number" || typeof vNode === "string") {
     return new Text(String(vNode));
   }
+
+  if (Array.isArray(vNode)) {
+    const fragment = new DocumentFragment();
+
+    vNode.forEach((node) => fragment.append(createElement(node)));
+
+    return fragment;
+  }
+
+  if (!Array.isArray(vNode) && typeof vNode === "object") {
+    const container = document.createElement(vNode.type);
+
+    Object.entries(vNode?.props ?? []).forEach(([attr, value]) => {
+      container.setAttribute(attr, value);
+    });
+
+    container.textContent = vNode.children?.join("");
+
+    return container;
+  }
 }
 
 function updateAttributes($el, props) {}
