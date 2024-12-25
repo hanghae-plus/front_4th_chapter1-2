4d4c37e6b05e398fe41653c0956faf907f7d67f8
DESKTOP-PPQ6A24\USER <thddl2079@naver.com>
2024-12-26 00:23:23 +0900

    심화과제 > 포스트관리 > 비회원

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -2,13 +2,28 @@
 import { createVNode } from "../../lib";
 import { toTimeFormat } from "../../utils/index.js";
 
+import { globalStore } from "../../stores";
+
 export const Post = ({
+  id,
   author,
   time,
   content,
   likeUsers,
   activationLike = false,
 }) => {
+  const { loggedIn } = globalStore.getState();
+  const { toggleLike } = globalStore.actions;
+
+  const likeToggle = () => {
+    if (!loggedIn) {
+      alert("로그인 후 이용해주세요");
+
+      return false;
+    }
+    toggleLike(id);
+  };
+
   return (
     <div className="bg-white rounded-lg shadow p-4 mb-4">
       <div className="flex items-center mb-2">
@@ -21,6 +36,7 @@ export const Post = ({
       <div className="mt-2 flex justify-between text-gray-500">
         <span
           className={`like-button cursor-pointer${activationLike ? " text-blue-500" : ""}`}
+          onClick={likeToggle}
         >
           좋아요 {likeUsers.length}
         </span>
