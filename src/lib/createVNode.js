23120fa9b01c944136e470deabf0ea2089fbd5c0
confidential-nt <youyoy689@gmail.com>
2024-12-25 19:57:49 +0900

    feat: createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,19 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  const flatChildren = children
+    .flat(Infinity)
+    .filter((child) => child || child === 0);
+
+  return {
+    type,
+    props,
+    children: flatChildren,
+  };
 }
+
+const vNode = createVNode(
+  "div",
+  null,
+  createVNode("span", null, "Hello"),
+  createVNode("b", null, "world"),
+);
+console.log(vNode);
