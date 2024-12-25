af45ad8c9088bbc026b5fd430e0665cebd325466
chb6734 <chb6734@gmail.com>
2024-12-25 18:20:28 +0900

    feat: normalizeVnode 정규화 test 통과

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,4 +1,6 @@
 export function normalizeVNode(vNode) {
+  //null, undefined, boolean 처리
+
   if (
     typeof vNode === "boolean" ||
     typeof vNode === "undefined" ||
@@ -6,6 +8,29 @@ export function normalizeVNode(vNode) {
   ) {
     return "";
   }
-  console.log(vNode);
-  return vNode;
+  // 문자열, 숫자 -> 문자열로 처리
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return vNode.toString();
+  }
+  // vNode가 function일 때
+  if (typeof vNode === "object") {
+    if (typeof vNode.type === "function") {
+      return normalizeVNode(
+        vNode.type({ ...vNode.props, children: vNode.children }),
+      );
+    }
+    if (Array.isArray(vNode.children)) {
+      return {
+        type: vNode.type,
+        props: { ...vNode.props },
+        children: vNode.children.map((child) => normalizeVNode(child)),
+      };
+    }
+  }
+
+  return {
+    type: vNode.type,
+    props: vNode.props,
+    children: vNode.children,
+  };
 }
