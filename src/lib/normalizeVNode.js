baa46a02ebc5975543fd603c5ff8ef7fe859a347
jch1223 <ico1828@gmail.com>
2024-12-26 00:40:41 +0900

    normalizedVNode 로직 작성

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,32 @@
 export function normalizeVNode(vNode) {
+  if (
+    typeof vNode === "boolean" ||
+    typeof vNode === "undefined" ||
+    vNode === null
+  ) {
+    return "";
+  }
+
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return String(vNode);
+  }
+
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(
+      vNode.type({
+        ...vNode.props,
+        children: normalizeVNode(vNode.children),
+      }),
+    );
+  }
+
+  if (Array.isArray(vNode)) {
+    return vNode.map(normalizeVNode);
+  }
+
+  if (Array.isArray(vNode.children)) {
+    vNode.children = vNode.children.filter(normalizeVNode);
+  }
+
   return vNode;
 }
