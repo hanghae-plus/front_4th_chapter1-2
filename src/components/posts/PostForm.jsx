154dc15e421b81217c76fb6cdb5fa559f603416a
KimYoungWoo <rladuddn3924@gmail.com>
2024-12-26 00:59:55 +0900

    FEAT: 게시물 작성 로직 추가

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,7 +1,25 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores/index.js";
 
 export const PostForm = () => {
+  const handleSubmit = () => {
+    const content = document.getElementById("post-content").value;
+    if (!content) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
+    const { posts, currentUser } = globalStore.getState();
+    const newPost = {
+      id: posts.length + 1,
+      author: currentUser?.username,
+      time: Date.now(),
+      content,
+      likeUsers: [],
+    };
+
+    globalStore.setState({ posts: [...posts, newPost] });
+  };
   return (
     <div className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
@@ -10,6 +28,7 @@ export const PostForm = () => {
         className="w-full p-2 border rounded"
       />
       <button
+        onClick={handleSubmit}
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
       >
