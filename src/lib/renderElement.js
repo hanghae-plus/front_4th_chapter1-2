8ad15f906d190477a1ffd9c1c8abad35213f8b98
jungeun0712 <kje970712@naver.com>
2024-12-25 21:41:11 +0900

    feat: render 기능 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -7,10 +7,14 @@ export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
-
   const normalizedVNode = normalizeVNode(vNode);
-  const element = createElement(normalizedVNode);
-  container.innerHTML = "";
-  container.appendChild(element);
+
+  if (container.firstChild) {
+    container.firstChild.replaceWith(createElement(normalizedVNode));
+    // updateElement
+  } else {
+    const element = createElement(normalizedVNode);
+    container.appendChild(element);
+  }
   setupEventListeners(container);
 }
