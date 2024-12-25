d87ca6eefae947352eca318a85cbce68ea85f299
nogy21 <nogy21@gmail.com>
2024-12-25 22:19:05 +0900

    feat: on 이벤트 속성 추가/제거 함수 및 on 이벤트 속성명 추출 함수 추가
    - updateElement 함수에서 기존 노드 제거 시 removeOnEvent 호출, 신규 노드 추가 시 addOnEvent 호출하도록 변경

--- src/lib/updateElement.js
+++ src/lib/updateElement.js
@@ -22,17 +22,17 @@ function updateAttributes(target, originNewProps, originOldProps) {
   const props = { ...newProps, ...oldProps };
 
   Object.keys(props).forEach((key) => {
-    if (key.startsWith("on")) {
-      const eventType = key.slice(2).toLowerCase();
-      if (newProps[key] && !oldProps[key]) {
-        addEvent(target, eventType, newProps[key]);
-      } else if (!newProps[key] && oldProps[key]) {
-        removeEvent(target, eventType, oldProps[key]);
-      }
+    if (newProps[key] === oldProps[key]) {
       return;
     }
 
-    if (newProps[key] === oldProps[key]) {
+    const onEvent = getOnEventName(key);
+    if (onEvent) {
+      if (newProps[key] && !oldProps[key]) {
+        addEvent(target, onEvent, newProps[key]);
+      } else if (!newProps[key] && oldProps[key]) {
+        removeEvent(target, onEvent, oldProps[key]);
+      }
       return;
     }
 
@@ -61,11 +61,13 @@ function updateAttributes(target, originNewProps, originOldProps) {
 export function updateElement(parentElement, newNode, oldNode, index = 0) {
   // 신규 노드가 없는 경우 기존 노드 제거
   if (oldNode && !newNode) {
+    removeOnEvent(parentElement, oldNode);
     return parentElement.removeChild(parentElement.childNodes[index]);
   }
 
   // 기존 노드가 없는 경우 신규 노드 추가
   if (!oldNode && newNode) {
+    addOnEvent(parentElement, newNode);
     return parentElement.appendChild(createElement(newNode));
   }
 
@@ -113,4 +115,53 @@ export function updateElement(parentElement, newNode, oldNode, index = 0) {
       i,
     );
   }
+}
+
+/**
+ * 이벤트 속성 추가 함수
+ * @param {*} parentElement
+ * @param {*} newNode
+ * @returns {void}
+ */
+export function addOnEvent(parentElement, newNode) {
+  if (!newNode.props) {
+    return;
+  }
+  Object.keys(newNode.props).forEach((key) => {
+    const eventType = getOnEventName(key);
+    if (eventType) {
+      addEvent(parentElement, eventType, newNode.props[key]);
+    }
+  });
+}
+
+/**
+ * 이벤트 속성 제거 함수
+ * @param {*} parentElement
+ * @param {*} oldNode
+ * @returns {void}
+ */
+export function removeOnEvent(parentElement, oldNode) {
+  if (!oldNode.props) {
+    return;
+  }
+  Object.keys(oldNode.props).forEach((key) => {
+    const eventType = getOnEventName(key);
+    if (eventType) {
+      removeEvent(parentElement, eventType, oldNode.props[key]);
+    }
+  });
+}
+
+/**
+ * 이벤트 속성명 추출 함수
+ * @description 속성명이 on으로 시작하는 경우 이벤트 속성명 반환, 그 외의 경우 null 반환
+ * @param {*} key
+ * @returns {string|null}
+ */
+export function getOnEventName(key) {
+  if (key.startsWith("on")) {
+    return key.slice(2).toLowerCase();
+  }
+  return null;
 }
