710bc7863513e7ddc369dc79e8a800c5ca7b50dc
jch1223 <ico1828@gmail.com>
2024-12-26 01:39:11 +0900

    text node 반환 로직

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,17 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  if (
+    typeof vNode === "boolean" ||
+    typeof vNode === "undefined" ||
+    vNode === null
+  ) {
+    return new Text("");
+  }
+
+  if (typeof vNode === "number" || typeof vNode === "string") {
+    return new Text(String(vNode));
+  }
+}
 
 function updateAttributes($el, props) {}
