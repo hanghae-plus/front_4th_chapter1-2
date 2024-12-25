1ff9d2a6f678890b43bb3afa67bc142b11a4b9b6
nogy21 <nogy21@gmail.com>
2024-12-26 02:46:16 +0900

    fix: normalizeVNode 함수 내 함수 타입 정규화 로직 개선 및 불필요한 함수 제거
    - 문제: jsx 컴포넌트 렌더링이 안되는 이슈
    - 상황: 함수형 컴포넌트에 대한 호출이 안됨
    - 분석: 노드를 정규화하는 과정에서 type이 함수형일 경우 재귀적 호출에 문제가 있는 것으로 파악. 또한, 자식 요소 정규화 과정에서 내부 구조가 적절하게 처리되지 않은 것으로 보임
    - 해결: 속성 및 자식 요소 처리 로직 단순화, 재귀 호출 로직 개선

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -6,7 +6,7 @@ import { isValid } from "../utils/isValid";
  * @description 조건에 따라 vNode를 정규화하고, 정규화된 vNode를 반환한다.
  * 1. vNode가 null, undefined, boolean인 경우 빈 문자열을 반환한다.
  * 2. vNode가 문자열 또는 숫자인 경우 해당 값을 문자열로 반환한다.
- * 3. vNode가 함수 타입인 경우 해당 함수를 호출하며 정규화된 vNode({type, props, children})를 반환한다.
+ * 3. vNode가 함수 타입인 경우 해당 함수를 호출하며 반환된 값을 정규화한다.
  * 4. 모든 자식 노드를 정규화한다.
  * @param {Object} vNode - 정규화할 가상 노드
  * @returns {Object|string} 정규화된 가상 노드 또는 vNode가 유효하지 않은 경우 빈 문자열을 반환
@@ -20,55 +20,14 @@ export function normalizeVNode(vNode) {
   }
 
   if (typeof vNode.type === "function") {
-    vNode = normalizeFunctionType(vNode);
+    return normalizeVNode(
+      vNode.type({ ...vNode.props, children: vNode.children }),
+    );
   }
 
   if (vNode.children && vNode.children.length) {
-    vNode.children = normalizeChildren(vNode.children) || [];
+    vNode.children = vNode.children.filter(isValid).map(normalizeVNode);
   }
 
   return vNode;
-}
-
-/**
- * 함수 타입 정규화
- * @description vNode가 함수 타입인 경우 해당 함수를 호출하며 정규화된 vNode({type, props, children})를 반환
- * children이 문자열 배열인 경우 문자열을 합쳐서 반환하고, 그 외의 경우는 normalizeChildren 함수를 통해 정규화
- * @param {Object} vNode
- * @returns {Object} 정규화된 vNode
- * @returns {string|Function} returns.type: normalizeVNode 함수 실행 결과의 type
- * @returns {Object} returns.props: 함수 실행 결과의 props
- * @returns {Array} returns.children: normalizeChildren 함수를 통해 children 요소 정규화
- */
-function normalizeFunctionType(vNode) {
-  const renderedVNode = vNode.type(vNode.props || {});
-
-  if (typeof renderedVNode.type === "function") {
-    renderedVNode.type = normalizeFunctionType(renderedVNode).type;
-  }
-
-  const mergedChildren = [
-    ...(renderedVNode.children || []),
-    ...(vNode.children || []),
-  ];
-
-  return {
-    type: renderedVNode.type,
-    props: renderedVNode.props || {},
-    children: normalizeChildren(mergedChildren),
-  };
-}
-
-/**
- * 자식 노드 정규화
- * @description 자식 노드 중 문자열이 연속되는 경우 합쳐서 반환하고, 그 외의 경우는 normalizeVNode 함수를 통해 정규화
- * @param {Array} children
- * @returns {Array} 정규화된 자식 노드
- */
-function normalizeChildren(children) {
-  if (!children || !children.length) return [];
-
-  return children
-    .filter(isValid)
-    .map((child) => (isString(child) ? child : normalizeVNode(child)));
 }
