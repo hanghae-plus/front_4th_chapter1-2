7983a16f0a87828799bc1c5114687659ac8f312e
KimYoungWoo <rladuddn3924@gmail.com>
2024-12-26 01:00:04 +0900

    FEAT: 좋아요 토글 로직 추가

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,14 +1,31 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
 import { toTimeFormat } from "../../utils/index.js";
+import { globalStore } from "../../stores/index.js";
 
-export const Post = ({
-  author,
-  time,
-  content,
-  likeUsers,
-  activationLike = false,
-}) => {
+export const Post = ({ author, time, content, likeUsers }) => {
+  const { loggedIn, currentUser, posts } = globalStore.getState();
+  const activationLike = likeUsers.includes(currentUser?.username);
+  const handleLike = () => {
+    if (!currentUser) {
+      alert("로그인 후 이용해주세요");
+      return;
+    }
+
+    const newPosts = posts.map((post) => {
+      if (post.author === author && post.time === time) {
+        return {
+          ...post,
+          likeUsers: activationLike
+            ? post.likeUsers.filter((user) => user !== currentUser.username)
+            : [...post.likeUsers, currentUser?.username],
+        };
+      }
+      return post;
+    });
+    globalStore.setState({ posts: newPosts });
+  };
+
   return (
     <div className="bg-white rounded-lg shadow p-4 mb-4">
       <div className="flex items-center mb-2">
@@ -20,6 +37,7 @@ export const Post = ({
       <p>{content}</p>
       <div className="mt-2 flex justify-between text-gray-500">
         <span
+          onClick={handleLike}
           className={`like-button cursor-pointer${activationLike ? " text-blue-500" : ""}`}
         >
           좋아요 {likeUsers.length}
