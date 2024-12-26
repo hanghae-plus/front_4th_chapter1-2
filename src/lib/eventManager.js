c8166744d9ab6e66e8719a118ef26434bb57fa90
suzy <susie0529@naver.com>
2024-12-27 00:34:35 +0900

    feat: chapter 1-2 test 통과

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,69 @@
-export function setupEventListeners(root) {}
+const eventListeners = new Map();
+let $root = null;
 
-export function addEvent(element, eventType, handler) {}
+function handleEvent(e) {
+  let target = e.target;
 
-export function removeEvent(element, eventType, handler) {}
+  while (target && target !== $root) {
+    const handlers = eventListeners.get(e.type)?.get(target);
+    if (handlers) {
+      handlers.forEach((handler) => handler(e));
+    }
+    target = target.parentElement;
+  }
+}
+
+export function setupEventListeners(root) {
+  if ($root) return;
+  $root = root;
+
+  eventListeners.forEach((handlers, eventType) => {
+    $root.removeEventListener(eventType, handleEvent);
+    $root.addEventListener(eventType, handleEvent);
+  });
+
+  // const supportedEvents = ["click", "input", "change", "submit"];
+
+  // supportedEvents.forEach((eventType) => {
+  //   if (!eventListeners.has(eventType)) {
+  //     eventListeners.set(eventType, new Map());
+  //   }
+
+  //   root.addEventListener(eventType, handleEvent, true);
+  // });
+}
+
+export function addEvent(element, eventType, handler) {
+  if (!eventListeners.has(eventType)) {
+    eventListeners.set(eventType, new WeakMap());
+  }
+
+  const handlers = eventListeners.get(eventType);
+  if (!handlers.has(element)) {
+    handlers.set(element, new Set());
+  }
+
+  handlers.get(element).add(handler);
+}
+
+export function removeEvent(element, eventType, handler) {
+  const handlers = eventListeners.get(eventType);
+
+  if (!handlers) return;
+
+  const handlerList = handlers.get(element);
+  if (handlerList) {
+    handlerList.delete(handler);
+    if (handlerList.size === 0) {
+      handlers.delete(element);
+    }
+  }
+
+  if (eventListeners.size === 0) {
+    eventListeners.delete(eventType);
+    if ($root && $root._listeners?.has(eventType)) {
+      $root.removeEventListener(eventType, handleEvent, true);
+      $root._listeners.delete(eventType);
+    }
+  }
+}
