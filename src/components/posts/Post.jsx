594ca22698c451e9c65bfa004f6bc97ee1451ce5
nogy21 <nogy21@gmail.com>
2024-12-26 15:27:22 +0900

    feat: 홈페이지 로그인 여부에 따른 게시물 조작 구현
    - 전역 정보의 로그인 한 사용자, 현재 사용자 정보를 이용해 권한에 따른 게시물 조작 기능 처리
      - 로그인하지 않은 사용자는 게시물 등록 폼을 노출하지 않음
      - 로그인하지 않은 사용자가 게시물 좋아요 누를 시 alert 노출
      - 로그인한 사용자는 게시물 추가 및 좋아요 클릭 가능

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,14 +1,42 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores/index.js";
 import { toTimeFormat } from "../../utils/index.js";
 
 export const Post = ({
+  id,
   author,
   time,
   content,
   likeUsers,
   activationLike = false,
 }) => {
+  const { currentUser } = globalStore.getState();
+
+  const handleLike = () => {
+    if (!currentUser) {
+      alert("로그인 후 이용해주세요");
+      return;
+    }
+    const { posts } = globalStore.getState();
+    const updatedPosts = posts.map((post) => {
+      if (post.id !== id) {
+        return post;
+      }
+      const username = currentUser?.username;
+      const alreadyLiked = post.likeUsers.find((name) => name === username);
+      const newPost = {
+        ...post,
+        likeUsers: alreadyLiked
+          ? post.likeUsers.filter((user) => user !== username)
+          : [...post.likeUsers, username],
+      };
+      return { ...newPost };
+    });
+
+    globalStore.setState({ posts: updatedPosts });
+  };
+
   return (
     <div className="bg-white rounded-lg shadow p-4 mb-4">
       <div className="flex items-center mb-2">
@@ -21,6 +49,10 @@ export const Post = ({
       <div className="mt-2 flex justify-between text-gray-500">
         <span
           className={`like-button cursor-pointer${activationLike ? " text-blue-500" : ""}`}
+          onClick={(e) => {
+            e.preventDefault();
+            handleLike();
+          }}
         >
           좋아요 {likeUsers.length}
         </span>
