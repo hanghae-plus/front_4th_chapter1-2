160ca5f99d62a554fba6fd637ca60c20a2012212
lshyun955 <lshyun955@gmail.com>
2024-12-27 04:10:38 +0900

    수정: 이벤트 관리 파일 작성 완료

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,29 @@
-export function setupEventListeners(root) {}
+const eventHandlers = {};
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  Object.keys(eventHandlers).forEach((eventType) => {
+    root.addEventListener(eventType, (event) => {
+      const handler = eventHandlers[event.type].get(event.target);
+      if (!handler) {
+        return;
+      }
+      handler(event);
+    });
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+// addEvent와 removeEvent는 동일하게 유지
+export function addEvent(element, eventType, handler) {
+  let elementHandlers = eventHandlers[eventType];
+  if (!elementHandlers) {
+    eventHandlers[eventType] = new WeakMap();
+  }
+
+  eventHandlers[eventType].set(element, handler);
+}
+
+export function removeEvent(element, eventType) {
+  if (eventHandlers[eventType] && eventHandlers[eventType].has(element)) {
+    eventHandlers[eventType].delete(element);
+  }
+}
