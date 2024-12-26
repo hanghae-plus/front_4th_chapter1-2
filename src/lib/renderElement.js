4a16cbb6162a6d129fd1c424e8fa7426501defd3
pitang <wpeffect@gmail.com>
2024-12-27 04:17:49 +0900

    fix: 심화 과제에 맞춰 renderElement 수정

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,25 +1,23 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-// import { updateElement } from "./updateElement";
+import { updateElement } from "./updateElement";
 
-// let currentVNode = null;
+let currentVNode = new WeakMap();
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
   const normalizedVNode = normalizeVNode(vNode);
 
   // 기존 DOM 제거
-  if (container.firstChild) {
-    container.removeChild(container.firstChild);
+  if (!currentVNode.has(container)) {
+    const $el = createElement(normalizedVNode);
+    container.appendChild($el);
+  } else {
+    const oldNode = currentVNode.get(container);
+    updateElement(container, normalizedVNode, oldNode);
   }
-
-  // 새로운 DOM 생성
-  const $el = createElement(normalizedVNode);
-  container.appendChild($el);
 
   // 이벤트 리스너 설정
   setupEventListeners(container);
+  currentVNode.set(container, normalizedVNode);
 }
