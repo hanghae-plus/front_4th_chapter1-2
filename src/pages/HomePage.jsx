e5ec74fe7584d13a447e203b108234e41a0bf633
guswls461419 <guswls461419@gmail.com>
2024-12-27 02:26:05 +0900

    fix: globalStore의 currentUser값 사용하도록 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import { createVNode } from "../lib";
 
 import { Footer, Header, Navigation, Post, PostForm } from "../components";
 import { globalStore } from "../stores";
-import { userStorage } from "../storages";
 
 /**
  * 심화과제
@@ -12,11 +11,11 @@ import { userStorage } from "../storages";
  * - 로그인하지 않은 사용자가 게시물에 좋아요를 누를 경우, "로그인 후 이용해주세요"를 alert로 띄운다.
  */
 export const HomePage = () => {
-  const { posts, loggedIn } = globalStore.getState();
+  const { posts, loggedIn, currentUser } = globalStore.getState();
 
   const isChecked = (likeUsers) => {
     return likeUsers.find((postLike) =>
-      postLike.includes(userStorage.get("user")?.username),
+      postLike.includes(currentUser.username),
     );
   };
 
