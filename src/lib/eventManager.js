a1ca31f85e307bf0999620a97c47c62205556378
chb6734 <chb6734@gmail.com>
2024-12-27 08:13:37 +0900

    feat: addEvent 테스트 통과

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,5 +1,42 @@
-export function setupEventListeners(root) {}
+let events = new Map();
 
-export function addEvent(element, eventType, handler) {}
+export function setupEventListeners(root) {
+  events.forEach((element) => {
+    element.forEach((eventType, handler) => {
+      root.addEventListener(handler, eventType);
+      console.log("====================================");
+      console.log("root eVent : ", root);
+      console.log("====================================");
+    });
+  });
+}
 
-export function removeEvent(element, eventType, handler) {}
+//createElement("button") === createElement("button") 인가?
+// ==> nope 각각 다른 객체
+
+// 엘리먼트 별로 핸들러를 관리하도록 해보기 ==> 통과
+
+// 엘리먼트별로 핸들러를 관리하는데 버블링 우째처리하노...
+// 각각 처리되도록 해야하나...?
+
+export function addEvent(element, eventType, handler) {
+  if (!events.has(element)) {
+    events.set(element, new Map());
+  }
+
+  if (!events.get(element).has(eventType)) {
+    events.get(element).set(eventType, handler);
+  }
+}
+
+export function removeEvent(element, eventType, handler) {
+  if (events.has(element) && events.get(element).has(eventType)) {
+    events.get(element).delete(eventType);
+    console.log("====================================");
+    console.log("events: ", events.get(element).has(handler));
+    console.log("====================================");
+    if (events.get(element).size === 0) {
+      events.delete(element);
+    }
+  }
+}
