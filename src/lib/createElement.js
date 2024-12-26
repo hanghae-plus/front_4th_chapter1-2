c460fc6ba1f7930b72a4963d906c53d7ba9a6879
effozen <ashgrayblue0@gmail.com>
2024-12-26 19:29:59 +0900

    [Feat] : '컴포넌트를 정규화 한다.' 라는 부분을 제외하고는 createElement나 normalizeVNode 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,48 @@
 import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+// function updateAttributes($el, props) {}
 
-function updateAttributes($el, props) {}
+export function createElement(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return document.createTextNode("");
+
+  if (typeof vNode === "string" || typeof vNode === "number")
+    return document.createTextNode(String(vNode));
+
+  if (vNode && Array.isArray(vNode)) {
+    const fragment = document.createDocumentFragment();
+
+    const children = vNode.map((child) => createElement(child));
+
+    children.forEach((child) => {
+      fragment.appendChild(child);
+    });
+
+    return fragment;
+  }
+
+  const $el = document.createElement(vNode.type);
+
+  if (vNode.props) {
+    Object.keys(vNode.props).forEach((key) => {
+      // 리액트 이벤트 규칙에는 on으로 시작해야한다는 규칙이 있다.
+      // 정규표현식으로 대문자도 체크할까하다가 Tomuch인거 같아서 패스.
+      if (key.startsWith("on")) {
+        const eventType = key.slice(2).toLowerCase();
+        addEvent(eventType, vNode.props[key], vNode.props[key]);
+        return;
+      }
+
+      $el.setAttribute(key, vNode.props[key]);
+    });
+  }
+
+  if (vNode.children) {
+    vNode.children.forEach((child) => {
+      const $child = createElement(child);
+      $el.appendChild($child);
+    });
+  }
+
+  return $el;
+}
--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,25 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean")
+    return "";
+
+  if (typeof vNode === "string" || typeof vNode === "number")
+    return String(vNode);
+
+  if (typeof vNode === "function") {
+    return normalizeVNode(vNode());
+  }
+
+  if (typeof vNode === "object") {
+    if (vNode.children && Array.isArray(vNode.children)) {
+      vNode.children = vNode.children
+        .map((child) => normalizeVNode(child))
+        .filter(
+          (child) => child !== null && child !== undefined && child !== "",
+        );
+    }
+
+    return vNode;
+  }
+
+  return "";
 }
