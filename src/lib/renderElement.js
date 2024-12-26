7495da325ea78796c2390202fcac2f6875e450de
lshyun955 <lshyun955@gmail.com>
2024-12-27 05:10:23 +0900

    수정: 요소 렌더 함수 구현

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -3,8 +3,23 @@ import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
 import { updateElement } from "./updateElement";
 
+const oldNodeMap = new WeakMap();
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  // 1. vNode 정규화
+  const normalizedVNode = normalizeVNode(vNode);
+
+  // 1-1. 이전 vNode 가져오기
+  const oldVNode = oldNodeMap.get(container);
+
+  if (oldVNode) {
+    // 이전 가상 DOM이 있으면 비교 후 업데이트
+    updateElement(container, normalizedVNode, oldVNode);
+  } else {
+    // 최초 렌더링
+    container.appendChild(createElement(normalizedVNode));
+  }
+
+  oldNodeMap.set(container, normalizedVNode);
+
+  setupEventListeners(container);
 }
