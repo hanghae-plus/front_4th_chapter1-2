07795b4144b28ffaeab092f453270ee6f4090ff4
pitang <wpeffect@gmail.com>
2024-12-27 04:18:30 +0900

    feat: 포스트 추가 기능 구현

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,9 +1,24 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
+
+const addPost = (e) => {
+  e.preventDefault();
+  const contentInput = document.getElementById("post-content");
+  const content = contentInput.value.trim(); // const 선언 유지
+  if (!content) {
+    return;
+  }
+
+  const { addPost } = globalStore.actions;
+  addPost(content);
+
+  contentInput.value = ""; //초기화
+};
 
 export const PostForm = () => {
   return (
-    <div className="mb-4 bg-white rounded-lg shadow p-4">
+    <form className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
         id="post-content"
         placeholder="무슨 생각을 하고 계신가요?"
@@ -12,9 +27,10 @@ export const PostForm = () => {
       <button
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+        onClick={addPost}
       >
         게시
       </button>
-    </div>
+    </form>
   );
 };
