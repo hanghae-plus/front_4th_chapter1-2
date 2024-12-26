37eadd03b69614d61fa2dddd7e193d850e02b308
Dayglow0926 <dayglow0926@gmail.com>
2024-12-27 04:11:04 +0900

    feat: createElement 속성 추가

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,7 @@
 // import { addEvent } from "./eventManager";
 
+import { addEvent } from "./eventManager";
+
 /**
  *
  * @param {*} vNode
@@ -29,9 +31,9 @@ export function createElement(vNode) {
     return document.createTextNode("");
   } else if (typeof vNode.type === "function") {
     //vNode.type이 function 일때
-    console.log(vNode);
-    const vNodeFn = vNode.type({ children: vNode.children, ...vNode.props });
-    return createElement(vNodeFn);
+    throw new Error();
+    // const vNodeFn = vNode.type({ children: vNode.children, ...vNode.props });
+    // return createElement(vNodeFn);
   }
 
   // tag에 대한 element를 만든다
@@ -40,7 +42,18 @@ export function createElement(vNode) {
   // tag의 props 데이터 setting
   Object.entries(vNode.props || {})
     .filter(([_, value]) => value) // eslint-disable-line no-unused-vars
-    .forEach(([attr, value]) => $el.setAttribute(attr, value));
+    .forEach(([attr, value]) => {
+      if (attr.startsWith("on")) {
+        // on 이벤트 root 위임
+        const eventType = attr.toLowerCase().substring(2);
+        addEvent($el, eventType, value);
+      } else if (attr === "className") {
+        //className => class 로 속성 넣기
+        $el.setAttribute("class", value);
+      } else {
+        $el.setAttribute(attr, value);
+      }
+    });
 
   // vNode의 children 을 재귀호출을 통해 dom 으로 구현
   // map을 통해 순회처리
