25fa01a840f72bb961dd10fc103265014c03c7bf
effozen <ashgrayblue0@gmail.com>
2024-12-26 18:30:45 +0900

    [Feat] : createVNode 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,11 @@
+// TODO : .flat이 성능적으로 좋지 않다. 최적화가 필요하다.
+// https://velog.io/@milkcoke/Javascript-Array.flat-%EC%9D%80-%EB%8A%90%EB%A6%AC%EB%8B%A4 참고
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children
+      .flat(Infinity)
+      .filter((child) => child === 0 || Boolean(child)),
+  };
 }
