e1f9a4d65977092f5f4893e009c3e10cc8fc84bd
nogy21 <nogy21@gmail.com>
2024-12-26 15:28:06 +0900

    feat: 게시물 제출 이벤트 처리 구현

--- src/components/posts/PostForm.jsx
+++ src/components/posts/PostForm.jsx
@@ -1,9 +1,31 @@
 /** @jsx createVNode */
 import { createVNode } from "../../lib";
+import { globalStore } from "../../stores";
 
 export const PostForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const content = document.getElementById("post-content").value;
+    if (!content) {
+      return;
+    }
+
+    globalStore.setState({
+      posts: [
+        ...globalStore.getState().posts,
+        {
+          id: globalStore.getState().posts.length + 1,
+          author: globalStore.getState().currentUser.username,
+          time: Date.now(),
+          content,
+          likeUsers: [],
+        },
+      ],
+    });
+  };
+
   return (
-    <div className="mb-4 bg-white rounded-lg shadow p-4">
+    <form className="mb-4 bg-white rounded-lg shadow p-4">
       <textarea
         id="post-content"
         placeholder="무슨 생각을 하고 계신가요?"
@@ -12,9 +34,10 @@ export const PostForm = () => {
       <button
         id="post-submit"
         className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+        onClick={handleSubmit}
       >
         게시
       </button>
-    </div>
+    </form>
   );
 };
