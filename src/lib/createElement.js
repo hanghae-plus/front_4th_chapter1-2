f00b0eefe4210890837a1d714a93705396cc2c50
9yurilee <9yurilee@gmail.com>
2024-12-27 14:24:16 +0900

    docs: 주석 정리

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -20,12 +20,12 @@ export function createElement(vNode) {
     return docFragment;
   }
 
-  // 4. 위 경우가 아니면 실제 DOM 요소를 생성.
-  // - vNode.type에 해당하는 요소를 생성
+  // 위 경우가 아니면 실제 DOM 요소를 생성.
+  // - vNode.type에 해당하는 요소를 생성.
   const element = document.createElement(vNode.type);
-  // - vNode.props의 속성들을 적용 (이벤트 리스너, className, 일반 속성 등 처리)
+  // - vNode.props의 속성들을 적용. (이벤트 리스너, className, 일반 속성 등 처리)
   handleUpdateAttr(element, vNode.props, {}); // props: { id: '' , ... }
-  // - vNode.children의 "각 자식"에 대해 createElement를 재귀 호출하여 추가
+  // - vNode.children의 "각 자식"에 대해 createElement를 재귀 호출하여 추가.
   element.append(...vNode.children.map(createElement));
 
   return element;
--- src/lib/createHashRouter.js
+++ src/lib/createHashRouter.js
@@ -3,9 +3,9 @@ import { createObserver } from "./createObserver";
 export const createHashRouter = (routes) => {
   const { subscribe, notify } = createObserver();
 
-  // 현재 해시 경로를 가져오는 함수
+  // 현재 해시 경로를 가져오는 함수.
   const getPath = () => {
-    // 해시가 없으면 '/'를, 있으면 '#' 이후의 경로를 반환
+    // 해시가 없으면 '/'를, 있으면 '#' 이후의 경로를 반환.
     return window.location.hash ? window.location.hash.slice(1) : "/";
   };
 
@@ -15,10 +15,10 @@ export const createHashRouter = (routes) => {
     window.location.hash = path;
   };
 
-  // 해시 변경 이벤트 리스너
+  // 해시 변경 이벤트 리스너.
   window.addEventListener("hashchange", notify);
 
-  // 초기 로드 시 해시가 없는 경우를 처리
+  // 초기 로드 시 해시가 없는 경우를 처리.
   window.addEventListener("load", () => {
     if (!window.location.hash) {
       push("/");
