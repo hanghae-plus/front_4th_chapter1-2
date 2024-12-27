8af67f82111cf3aa86f4d4f9172e6c519a72a50e
suzy <susie0529@naver.com>
2024-12-27 11:49:07 +0900

    fix: 코드 정리

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -21,16 +21,6 @@ export function setupEventListeners(root) {
     $root.removeEventListener(eventType, handleEvent);
     $root.addEventListener(eventType, handleEvent);
   });
-
-  // const supportedEvents = ["click", "input", "change", "submit"];
-
-  // supportedEvents.forEach((eventType) => {
-  //   if (!eventListeners.has(eventType)) {
-  //     eventListeners.set(eventType, new Map());
-  //   }
-
-  //   root.addEventListener(eventType, handleEvent, true);
-  // });
 }
 
 export function addEvent(element, eventType, handler) {
@@ -67,44 +57,3 @@ export function removeEvent(element, eventType, handler) {
     eventListeners.delete(eventType);
   }
 }
-
-// export function removeEvent(element, eventType, handler) {
-//   const elementEvents = eventListeners.get(element);
-//   if (!elementEvents) return;
-
-//   if (handler) {
-//     const handlers = elementEvents.get(eventType);
-//     if (handlers) {
-//       handlers.delete(handler);
-//       if (handlers.size === 0) {
-//         elementEvents.delete(eventType);
-//       }
-//     }
-//   } else {
-//     elementEvents.delete(eventType);
-//   }
-
-//   if (elementEvents.size === 0) {
-//     eventListeners.delete(element);
-//   }
-
-//   // const handlers = eventListeners.get(eventType);
-
-//   // if (!handlers) return;
-
-//   // const handlerList = handlers.get(element);
-//   // if (handlerList) {
-//   //   handlerList.delete(handler);
-//   //   if (handlerList.size === 0) {
-//   //     handlers.delete(element);
-//   //   }
-//   // }
-
-//   // if (eventListeners.size === 0) {
-//   //   eventListeners.delete(eventType);
-//   //   if ($root && $root._listeners?.has(eventType)) {
-//   //     $root.removeEventListener(eventType, handleEvent, true);
-//   //     $root._listeners.delete(eventType);
-//   //   }
-//   // }
-// }
