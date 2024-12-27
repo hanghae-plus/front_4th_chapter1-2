6ba0d49907694870a02b0d30de646a267aa2e63d
chb6734 <chb6734@gmail.com>
2024-12-27 21:28:00 +0900

    refactor: eventManager 주석 추가

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -3,6 +3,7 @@ let events = new Map();
 let oldRoot = null;
 
 export function setupEventListeners(root) {
+    // 초기등록 로직
     if (oldRoot !== root) {
         oldRoot = root;
         events.forEach((element) => {
@@ -10,12 +11,15 @@ export function setupEventListeners(root) {
                 root.addEventListener(handler, eventType);
             });
         });
-    } else {
+    } //이미 등록한적이 있을 때
+    else {
+        //지우고
         events.forEach((element) => {
             element.forEach((eventType, handler) => {
-                oldRoot.addEventListener(handler, eventType);
+                oldRoot.removeEventListener(handler, eventType);
             });
         });
+        //다시 등록
         events.forEach((element) => {
             element.forEach((eventType, handler) => {
                 root.addEventListener(handler, eventType);
@@ -43,21 +47,23 @@ export function addEvent(element, eventType, handler) {
 }
 
 export function removeEvent(element, eventType, handler) {
+    // handler변수 안쓰지만 테스트 코드 바꾸기 귀찮아서 로그로 사용
     console.log(handler);
+    //root에 저장된 모든 이벤트 삭제
     events.forEach((element) => {
         element.forEach((eventType, handler) => {
             oldRoot.removeEventListener(handler, eventType);
         });
     });
-
+    // Map에 저장된 해당 이벤트 삭제
     if (events.has(element) && events.get(element).has(eventType)) {
         events.get(element).delete(eventType);
-
+        // 만약 삭제 후 해당 엘리먼트에 어떤 이벤트도 없으면 엘리먼트도 삭제
         if (events.get(element).size === 0) {
             events.delete(element);
         }
 
-        // 이벤트 다시 등록
+        // root에 이벤트 다시 등록
         events.forEach((element) => {
             element.forEach((eventType, handler) => {
                 oldRoot.addEventListener(handler, eventType);
